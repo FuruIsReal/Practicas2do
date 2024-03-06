52be0996601daa400b1a42a8eae22770de982440
Alejandro Medina <drolejan@gmail.com>
2024-03-06 11:55:33 -0600

    avancesMiercoles6

--- Assets/PARCIAL 2/controlPlayer.cs
+++ Assets/PARCIAL 2/controlPlayer.cs
@@ -12,6 +12,7 @@ public class controlPlayer : MonoBehaviour
     public GameObject balaprefab;
     Vector2 movPlayer;
     public Transform spawnBala;
+    bool facingRight=true;
     void Start()
     {
         rbPlayer = GetComponent<Rigidbody2D>();//Obtenemos el RB para movernos
@@ -32,10 +33,12 @@ public class controlPlayer : MonoBehaviour
             if(Input.GetAxis("Horizontal") > 0)
             {
                 transform.localScale=new Vector3(1, 1, 1);
+                facingRight = true;
             }
             else if (Input.GetAxis("Horizontal") < 0)
             {
                 transform.localScale = new Vector3(-1, 1, 1);
+                facingRight = false;
             }
         }
         else
@@ -53,7 +56,7 @@ public class controlPlayer : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.F))
         {
             animPlayer.SetBool("shoot",true);
-            Instantiate(balaprefab,spawnBala.position,Quaternion.identity);
+            //Instantiate(balaprefab,spawnBala.position,Quaternion.identity);
         }
         if (Input.GetKeyUp(KeyCode.F))
         {
@@ -61,6 +64,17 @@ public class controlPlayer : MonoBehaviour
         }
 
     }
+
+    public void ataquePlayer()
+    {
+        GameObject bala=Instantiate(balaprefab, spawnBala.position, Quaternion.identity);
+        if (facingRight==false)
+        {
+            bala.GetComponent<bala>().balaSpeed *= -1;
+        }
+    }
+
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("suelo")){
